535f19df2cc170e3639eab5cfeb1da5dcdd239f2
uramer <antonuramer@gmail.com>
2020-02-01 15:32:44 +0100

    Refactor guard behaviour

--- GlobalGameJam2020/Assets/Code/GuardMovement.cs
+++ GlobalGameJam2020/Assets/Code/GuardMovement.cs
@@ -7,72 +7,134 @@ public class GuardMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float distance;
     [SerializeField] private GameObject bullet;
-    [SerializeField] private GameObject[] nodes;
+    [SerializeField] private GameObject[] waypoints;
     [SerializeField] private float[] waitTime;
 
-    [SerializeField] private GameObject[] players;
-    [SerializeField] private float distanceToTarget;
-    public Vector3 target;
+    [SerializeField] private float shotDelay;
 
-    private int counter = 0;
-    System.DateTime now;
+    [SerializeField] private GameObject[] units;
+    [SerializeField] private float chaseDistance;
+    [SerializeField] private float shootDistance;
+    public GameObject target;
+    private int currentWaypoint = 0;
+    private float patrolWaitTime = 0;
+    private float shotWaitTime = 0;
+    private enum State {
+        Patrol,
+        Chase,
+        Shoot,
+        Return
+    }
 
-    private string[] movementTypes = { "patrol", "chase" };
-    private int chosenMovement = 0;
+    private State state = State.Patrol;
 
-    private void Movement()
+    private void MoveTo(GameObject target) {
+        this.transform.position += speed * Time.deltaTime * (target.transform.position - this.transform.position).normalized;
+    }
+
+    private float DistanceFromMe(GameObject a) {
+        return (a.transform.position - this.gameObject.transform.position).magnitude;
+    }
+
+    private GameObject ChooseClosest()
     {
-        
-        if (Mathf.Abs(nodes[counter].transform.position.x - this.transform.position.x) >= distance && Mathf.Abs(nodes[counter].transform.position.y - this.transform.position.y) >= distance)
+        GameObject closest = null;
+        float closestDistance = 0f;
+        foreach (GameObject a in units)
         {
-            this.transform.position += speed * (nodes[counter].transform.position - this.transform.position).normalized * Time.deltaTime;
-            now = System.DateTime.Now.AddSeconds(waitTime[counter]);
-        }
-        else
-        {
-            if (System.DateTime.Now >= now)
-            {
-                if (counter >= nodes.Length - 1)
-                    counter = 0;
-                else
-                    counter++;
+            if(a == this.gameObject) continue;
+            float distance = DistanceFromMe(a);
+            if(closest == null || closestDistance > distance) {
+                closest = a;
+                closestDistance = distance;
             }
         }
+            
+        return closest;
     }
 
     private void Detect()
     {
-        if ((target - this.transform.position).magnitude < distanceToTarget)
-            chosenMovement = 1;
+        target = ChooseClosest();
+        if (DistanceFromMe(target) < chaseDistance)
+            state = State.Chase;
+    }
+
+    private void Patrol()
+    {
+        if (DistanceFromMe(waypoints[currentWaypoint]) >= distance)
+        {
+            MoveTo(waypoints[currentWaypoint]);
+            patrolWaitTime = waitTime[currentWaypoint];
+        }
         else
-            chosenMovement = 0;
+        {
+            if(patrolWaitTime <= 0) {
+                currentWaypoint++;
+                if (currentWaypoint >= waypoints.Length)
+                    currentWaypoint = 0;
+            }
+            else patrolWaitTime -= Time.deltaTime;
+        }
     }
 
     private void Chase()
     {
-        if ((target - this.transform.position).magnitude > 0.25 * distanceToTarget)
-            this.transform.position += 2 * speed * (target - this.transform.position).normalized * Time.deltaTime;
-
-        //if ((target - this.transform.position).magnitude < 0.05 * distanceToTarget)
-         //   Instantiate(bullet, this.transform.position, this.transform.rotation, this.transform);   
+        if (DistanceFromMe(target) > shootDistance * 0.8)
+            MoveTo(target);
+        else {
+            shotWaitTime = shotDelay;
+            state = State.Shoot;
+        }
     }
 
-    private void ChooseClosest()
-    {
-        Vector3 closestPlayer = new Vector3(100, 100, 100);
-        foreach (var a in players)
-            if ((closestPlayer - a.transform.position).magnitude > 0)
-                closestPlayer = a.transform.position;
-        target = closestPlayer;
+    private void Shoot() {
+        shotWaitTime -= Time.deltaTime;
+        if(shotWaitTime <= 0) {
+            if (DistanceFromMe(target) < shootDistance) 
+                Instantiate(
+                    bullet,
+                    this.transform.position + 0.1f * (target.transform.position - this.transform.position),
+                    this.transform.rotation,
+                    this.transform
+                );
+
+            state = State.Return;
+        }
+    }
+
+    private void Return() {
+        int nearestWaypoint = 0;
+        float minDistance = DistanceFromMe(waypoints[nearestWaypoint]);
+        for(int i = 1; i < waypoints.Length; i++) {
+            float distance = DistanceFromMe(waypoints[i]);
+            if(distance < minDistance) {
+                nearestWaypoint = i;
+                minDistance = distance;
+            }
+        }
+        currentWaypoint = nearestWaypoint;
+
+        state = State.Patrol;
     }
 
     private void Update()
     {
-        Detect();
-
-        if (movementTypes[chosenMovement] == "patrol")
-            Movement();
-        else if (movementTypes[chosenMovement] == "chase")
-            Chase();
+        Debug.Log(state);
+        switch(state) {
+            case State.Patrol:
+                Patrol();
+                Detect();
+                break;
+            case State.Chase:
+                Chase();
+                break;
+            case State.Shoot:
+                Shoot();
+                break;
+            case State.Return:
+                Return();
+                break;
+        }
     }
 }
