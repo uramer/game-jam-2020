6219e1e7bc9d47f0af2117e86d5da86caa460c2b
Krebsys <57034917+Krebsys@users.noreply.github.com>
2020-02-01 10:27:37 +0100

    Guard choosing closest player

--- GlobalGameJam2020/Assets/Code/GuardMovement.cs
+++ GlobalGameJam2020/Assets/Code/GuardMovement.cs
@@ -3,29 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GuardMovement : MonoBehaviour
-{
+{    
     [SerializeField] private float speed;
     [SerializeField] private float distance;
     [SerializeField] private GameObject[] nodes;
+    [SerializeField] private float[] waitTime;
+
+    [SerializeField] private GameObject[] players;
+    [SerializeField] private float distanceToTarget;
+
     private int counter = 0;
+    System.DateTime now;
+
+    private string[] movementTypes = { "patrol", "chase" };
+    private int chosenMovement = 0;
 
     private void Movement()
     {
+        
         if (Mathf.Abs(nodes[counter].transform.position.x - this.transform.position.x) >= distance && Mathf.Abs(nodes[counter].transform.position.y - this.transform.position.y) >= distance)
         {
             this.transform.position += speed * (nodes[counter].transform.position - this.transform.position).normalized * Time.deltaTime;
+            now = System.DateTime.Now.AddSeconds(waitTime[counter]);
         }
         else
         {
-            if (counter >= nodes.Length - 1)
-                counter = 0;
-            else
-                counter++;
+            if (System.DateTime.Now >= now)
+            {
+                if (counter >= nodes.Length - 1)
+                    counter = 0;
+                else
+                    counter++;
+            }
         }
     }
 
-    void Update()
+    private void Detect()
     {
-        Movement();
+        Vector3 target = ChooseClosest();
+
+        if (Mathf.Pow(Mathf.Pow(target.x, 2) + Mathf.Pow(target.y, 2), -2) - Mathf.Pow(Mathf.Pow(this.transform.position.x, 2) + Mathf.Pow(this.transform.position.y, 2), -2) < distanceToTarget)
+            chosenMovement = 1;
+        else
+            chosenMovement = 0;
+    }
+
+    private void Chase()
+    {
+
+    }
+
+    private Vector3 ChooseClosest()
+    {
+        Vector3 closestPlayer = new Vector3(100, 100, 100);
+
+        foreach (var a in players)
+        {
+            if (Mathf.Pow(Mathf.Pow(closestPlayer.x, 2) + Mathf.Pow(closestPlayer.y, 2), -2) - Mathf.Pow(Mathf.Pow(a.transform.position.x, 2) + Mathf.Pow(a.transform.position.y, 2), -2) > 0)
+            {
+                closestPlayer = a.transform.position;
+            }
+        }
+        return closestPlayer;
+    }
+
+    private void Update()
+    {
+        Detect();
+
+        if (movementTypes[chosenMovement] == "patrol")
+            Movement();
+        else if (movementTypes[chosenMovement] == "chase")
+            Chase();
     }
 }
