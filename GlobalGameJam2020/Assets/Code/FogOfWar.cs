564d90ad035cdf6f1cb86a3113f1a2c454094522
Krebsys <57034917+Krebsys@users.noreply.github.com>
2020-02-01 23:47:35 +0100

    FOW start

---
+++ GlobalGameJam2020/Assets/Code/FogOfWar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWar : MonoBehaviour
+{
+    [SerializeField] private GameObject fogPlain;
+    [SerializeField] private GameObject[] players;
+    
+    
+}
