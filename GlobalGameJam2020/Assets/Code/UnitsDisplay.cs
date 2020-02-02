4e566e721056dca6158cf0420b99a9fe3b0c94df
bor brecelj <bor.brecelj@gmail.com>
2020-02-02 10:18:06 +0100

    HUD shows which units are selected

--- GlobalGameJam2020/Assets/Code/UnitsDisplay.cs
+++ GlobalGameJam2020/Assets/Code/UnitsDisplay.cs
@@ -11,75 +11,50 @@ public class UnitsDisplay : MonoBehaviour
 	[SerializeField] Canvas canvas;
 
 	private float canvasWidth, canvasHeight;
-	private List<GameObject> texts = new List<GameObject>();
-	//private List<GameObject> iconsOfUnits = new List<GameObject>();
-	private Dictionary<GameObject, GameObject> unitToIcon = new Dictionary<GameObject, GameObject>();
+	private Dictionary<GameObject, GameObject> unitToIcon = new Dictionary<GameObject, GameObject>();	
+	private Dictionary<GameObject, GameObject> unitToSelected = new Dictionary<GameObject, GameObject>();
 
     void Start()
     {
-    	//addText("Bla bla!", new Vector3(0, 0, 0), Color.black);
     	RectTransform rt = canvas.gameObject.GetComponent<RectTransform>();
     	canvasWidth = rt.rect.width;
     	canvasHeight = rt.rect.height;
-    	//Debug.Log(canvasWidth + " " + canvasHeight);
 
     	GameObject[] units = GameObject.FindGameObjectsWithTag("Player");
-    	//SpriteRenderer sr = units[0].transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
-    	//addIcon("icon", new Vector3(10-canvasWidth/2, 10-canvasHeight/2, 0), sr);
 
     	float pos = 10-canvasWidth/2;
     	foreach(GameObject unit in units) {
+    		Vector3 posVec = new Vector3(pos, 10-canvasHeight/2, 0);
     		SpriteRenderer sr = unit.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
-    		unitToIcon[unit] = addIcon("icon", new Vector3(pos, 10-canvasHeight/2, 0), sr);
+    		unitToSelected[unit] = addIcon("selected", posVec - new Vector3(5, 5, 0), 40, null, Color.red);
+    		unitToSelected[unit].SetActive(false);
+    		unitToIcon[unit] = addIcon("icon", posVec, 30, sr.sprite, sr.color);
     		pos += 50;
     	}
     }
 
     void Update()
     {
-    	/*foreach(GameObject t in texts) {
-    		Destroy(t);
+    	foreach(GameObject sel in unitToSelected.Values) {
+    		sel.SetActive(false);
     	}
-    	texts = new List<GameObject>();
-    	float pos = 20 - canvasWidth / 2;
-    	GameObject[] units = GameObject.FindGameObjectsWithTag("Player");
     	List<GameObject> selected = selectUnitsScript.GetSelected();
-    	foreach(GameObject unit in units) {
-    		if(selected.Contains(unit)) {
-    			texts.Add(addText(unit.name, new Vector3(pos, 20 - canvasHeight / 2, 0), Color.red));
-    		}
-    		else {
-    			texts.Add(addText(unit.name, new Vector3(pos, 20 - canvasHeight / 2, 0), Color.black));
-    		}
-    		pos += 120;
-    	}*/
+    	foreach(GameObject sel in selected) {
+    		unitToSelected[sel].SetActive(true);
+    	}
     }
 
-    GameObject addText(string txt, Vector3 pos, Color color) {
-    	GameObject text = new GameObject();
-    	text.transform.parent = canvas.transform;
-  		Text myText = text.AddComponent<Text>();
-    	myText.text = txt;
-    	myText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
-    	myText.color = color;
-    	RectTransform rectTransform = myText.GetComponent<RectTransform>();
-    	rectTransform.pivot = new Vector2(0, 0);
-        rectTransform.localPosition = pos;
-        rectTransform.sizeDelta = new Vector2(100, 20);
-        return text;
-    }
-
-    GameObject addIcon(string name, Vector3 pos, SpriteRenderer sr) {
+    GameObject addIcon(string name, Vector3 pos, float size, Sprite sprite, Color color) {
     	GameObject icon = new GameObject();
     	icon.name = name;
     	icon.transform.parent = canvas.transform;
     	Image image = icon.AddComponent<Image>();
-    	image.sprite = sr.sprite;
-    	image.color = sr.color;
+    	image.sprite = sprite;
+    	image.color = color;
     	RectTransform rectTransform = image.GetComponent<RectTransform>();
     	rectTransform.pivot = new Vector2(0, 0);
         rectTransform.localPosition = pos;
-        rectTransform.sizeDelta = new Vector2(30, 30);
+        rectTransform.sizeDelta = new Vector2(size, size);
         return icon;
     }
 }
